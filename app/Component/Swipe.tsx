b16bab6e215f79a0f1ff959588c91b81ebd80e9a
BakrimNadia <nadia_lina@hotmail.com>
2024-10-23 11:32:16 +0200

    WIP Create Product page and header componant

--- app/Component/Swipe.tsx
+++ app/Component/Swipe.tsx
@@ -1,27 +1,43 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 
-
 export default function Swipe() {
   return (
     <div className="bg-white">
-      <div className="mt-10">
+      <div className="mt-10 relative">
         <Swiper
           spaceBetween={50}
           slidesPerView={1}
-          pagination={{ clickable: true }}
-          className="h-100 w-150 border-radius-2" 
+          navigation={true} // Activer la navigation (flèches)
+          pagination={{ clickable: true }} // Pagination avec points
+          className="h-100 w-150 border-radius-2"
         >
           <SwiperSlide>
-            <img src="images/les-robes.png" alt="robe" className="w-full h-full object-cover" />
+            <img
+              src="images/les-robes.png"
+              alt="robe"
+              className="w-full h-full object-cover"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="images/sacs.png" alt="sacs" className="w-full h-full object-cover" />
+            <img
+              src="images/sacs.png"
+              alt="sacs"
+              className="w-full h-full object-cover"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="images/bijou2.png" alt="bijou" className="w-full h-full object-cover" />
+            <img
+              src="images/bijou2.png"
+              alt="bijou"
+              className="w-full h-full object-cover"
+            />
           </SwiperSlide>
         </Swiper>
+
+        {/* Flèches de navigation */}
+        <div className="swiper-button-prev" style={{ color: '#000' }}></div>
+        <div className="swiper-button-next" style={{ color: '#000' }}></div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,107 +1,18 @@
 'use client'
 
-import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Category from './Component/Category'
 import Swipe from './Component/Swipe'
 import Community from './Component/Community'
+import Header from './Component/Header'
 
 
-const navigation = [
-  { name: 'Nos Produits', href: '#' },
-  { name: 'Collection', href: '#' },
-  { name: 'A propos', href: '#' },
-  { name: 'Contact', href: '#' },
-]
 
 export default function Home() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
 
   return (
     <div className="bg-white">
-    <header className="absolute inset-x-0 top-0 z-50">
-      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-          </button>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-              {item.name}
-            </a>
-          ))}
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
+    <Header />
 
     <div className="relative isolate px-6 pt-14 lg:px-8">
       <div
@@ -152,13 +63,15 @@ export default function Home() {
           className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
         />
       </div>
-    </div> 
-    <div>
+      <div>
    <Category />
+   </div>
    <div>
    <Community />
    </div>
-  </div>
+    </div> 
+    
+  
   </div>
  
   );
