1c85b73fed67c1c79ba2fe70c1cfddb11e51ace0
BakrimNadia <nadia_lina@hotmail.com>
2025-02-13 13:33:49 +0100

    add picture and style to detail product

--- app/Detail-product/page.tsx
+++ app/Detail-product/page.tsx
@@ -31,8 +31,7 @@ const product = {
   price: '25 €',
   href: '#',
   breadcrumbs: [
-    { id: 1, name: 'women', href: '#' },
-    { id: 2, name: 'Clothing', href: '#' },
+    { id: 2, name: 'Vêtements', href: '#' },
   ],
   images: [
     {
@@ -56,15 +55,10 @@ const product = {
     { name: 'XL', inStock: true },
   ],
   description:
-    'Robe blanche bohême en coton, longueur 120 cm, largeur 50 cm, sans manches, col en V, broderies sur le devant.',
-  highlights: [
-    'Hand cut and sewn locally',
-    'Dyed with our proprietary colors',
-    'Pre-washed & pre-shrunk',
-    'Ultra-soft 100% cotton',
-  ],
+    'Robe blanche bohême en coton, existe en 2 coloris',
+
   details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+    'Robe blache style bohême, matière coton, longueur 120 cm, largeur 50 cm, sans manches, col en V, broderies sur le devant.',
 }
 const reviews = { href: '#', average: 4, totalCount: 117 }
 
@@ -112,12 +106,12 @@ export default function DetailProduct() {
         </nav>
 
         {/* Image gallery */}
-        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8 sm:grid-cols-1 md:grid-cols-2">
           <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
             <img
               alt={product.images[0].alt}
               src={product.images[0].src}
-              className="h-full w-full object-cover object-center"
+              className="h-full w-full object-cover object-center ease-in-out transform transition-all duration-150 hover:opacity-75"
             />
           </div>
           <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
@@ -125,7 +119,7 @@ export default function DetailProduct() {
               <img
                 alt={product.images[1].alt}
                 src={product.images[1].src}
-                className="h-full w-full object-cover object-center"
+                className="h-full w-full object-cover object-center ease-in-out transform transition-all duration-150 hover:opacity-75"
               />
             </div>
           </div>
@@ -139,12 +133,12 @@ export default function DetailProduct() {
 
           {/* Options */}
           <div className="mt-4 lg:row-span-3 lg:mt-0">
-            <h2 className="sr-only">Product information</h2>
+            <h2 className="sr-only">Information produit</h2>
             <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
 
             {/* Reviews */}
             <div className="mt-6">
-              <h3 className="sr-only">Reviews</h3>
+              <h3 className="sr-only">Avis</h3>
               <div className="flex items-center">
                 <div className="flex items-center">
                   {[0, 1, 2, 3, 4].map((rating) => (
@@ -160,7 +154,7 @@ export default function DetailProduct() {
                 </div>
                 <p className="sr-only">{reviews.average} out of 5 stars</p>
                 <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                  {reviews.totalCount} reviews
+                  {reviews.totalCount} Avis
                 </a>
               </div>
             </div>
@@ -251,7 +245,7 @@ export default function DetailProduct() {
 
               <button
                 type="submit"
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-pink-600 px-8 py-3 text-base font-medium text-white hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-rose-400 px-8 py-3 text-base font-medium text-white hover:bg-rose-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Ajouter au panier
               </button>
@@ -269,21 +263,7 @@ export default function DetailProduct() {
             </div>
 
             <div className="mt-10">
-              <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
-
-              <div className="mt-4">
-                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                  {product.highlights.map((highlight) => (
-                    <li key={highlight} className="text-gray-400">
-                      <span className="text-gray-600">{highlight}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-
-            <div className="mt-10">
-              <h2 className="text-sm font-medium text-gray-900">Details</h2>
+              <h2 className="text-sm font-medium text-gray-900">Détails</h2>
 
               <div className="mt-4 space-y-6">
                 <p className="text-sm text-gray-600">{product.details}</p>
